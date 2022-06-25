50bf6c8fb317d9ac439820ef4108bdf88f24f31b
adegokeadekunle2 <adegokeadekunle2@gmail.com>
2022-06-25 08:00:30 +0100

    added consumerdatabase

---
+++ kafka-consumer-database/src/main/java/com/adekunle/springboot/SpringBootConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.adekunle.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class SpringBootConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConsumerApplication.class);
+    }
+}
